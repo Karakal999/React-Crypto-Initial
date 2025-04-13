53fd74c9695226a2c64eeac13c042d8db3d1a06f
Karakal999 <valeriamagdenko573@gmail.com>
2025-04-13 20:19:30 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { CryptoContextProvider } from './context/cryptoContext';
 import AppLayout from './components/layout/AppLayout';
 
-
 export default function App() {
   return (
     <CryptoContextProvider>
